a025ddd7369de2fb18aa506d63deb1b939af9fe1
DaniellaMagalhaesSiqueira <daniella.siqueira80@gmail.com>
2022-10-20 18:53:01 -0300

    criação da classe Posicao

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using tabuleiro;
 
 namespace JogoXadrez {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            Posicao P;
+            P = new Posicao(3, 4);
+            Console.WriteLine($"Posicao {P}");
         }
     }
 }
