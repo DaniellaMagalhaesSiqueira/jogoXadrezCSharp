62e09fe7444338e0add5853caafa60dd584dd612
DaniellaMagalhaesSiqueira <daniella.siqueira80@gmail.com>
2022-10-21 19:00:49 -0300

    melhorando a implementacao no metodo imprimirtabuleiro

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -6,11 +6,21 @@ namespace JogoXadrez {
     class Program {
         static void Main(string[] args) {
 
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+            try {
 
-            Console.WriteLine(pos);
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(pos.toPosicao());
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(6, 2));
+                Tela.imprimirTabuleiro(tab);
+            } catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
