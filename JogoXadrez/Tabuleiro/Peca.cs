f431f416a59cb2c374d6488a8ab1f0290dd0bbc1
DaniellaMagalhaesSiqueira <daniella.siqueira80@gmail.com>
2022-10-22 10:10:41 -0300

    fazendo os primeiros testes de movimentacao

--- JogoXadrez/Tabuleiro/Peca.cs
+++ JogoXadrez/Tabuleiro/Peca.cs
@@ -23,5 +23,9 @@ namespace tabuleiro {
             this.cor = cor;
             this.qtdMovimentos = 0;
         }
+
+        public void incremetarMovimentos() {
+            qtdMovimentos++;
+        }
     }
 }
